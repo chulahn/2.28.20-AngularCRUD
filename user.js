e27accc8887d91bcbaf77d6498b89e76252bafbe
Chul Ahn <scahn.x@gmail.com>
2020-02-29 02:27:47 -0500

    make it persist

--- user.js
+++ user.js
@@ -1,11 +1,15 @@
 angular.module("userApp", []).controller("UserController", function() {
   var userList = this;
-  userList.users = [{ name: "George Washington" }, { name: "Chul Ahn" }];
+  userList.users = JSON.parse(localStorage.getItem("users")) || [
+    { name: "George Washington" },
+    { name: "Chul Ahn" }
+  ];
 
   userList.addUser = function() {
-    console.log(1);
     userList.users.push({
       name: userList.userName
     });
+
+    localStorage.setItem("users", JSON.stringify(userList.users));
   };
 });
